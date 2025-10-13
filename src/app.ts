130a7a1ee7b914782504e1ba89d1e8f08989ecc4
Escarcega <said.escarcega@accenture.com>
2025-10-13 12:48:22 -0600

    First commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application } from "express";
+import helloRoute from "./routes/hello.js";
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api", helloRoute);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+export default app;
---
+++ src/tests/app.test.js
@@ -0,0 +1,9 @@
+import request from "supertest";
+import app from "../app.js";
+describe("GET /api/hello", () => {
+    it("should return a greeting", async () => {
+        const res = await request(app).get("/api/hello");
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ message: "Hello, TypeScript world!" });
+    });
+});
